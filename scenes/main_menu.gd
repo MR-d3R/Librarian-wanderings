188a22a8a149605008c445dba8f0fdf0c425821d
Alexeev Eduard <47676479+MR-d3R@users.noreply.github.com>
2024-03-24 02:25:05 +0300

    Added main menu ad cursor

---
+++ scenes/main_menu.gd
@@ -0,0 +1,30 @@
+class_name MainMenu
+extends Control
+
+@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/start_button as Button
+@onready var settings_button = $MarginContainer/HBoxContainer/VBoxContainer/settings_button as Button
+@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/exit_button as Button
+
+@export var start_level = preload("res://scenes/world.tscn") as PackedScene
+var arrow = load("res://assests/textures/Cursor Default.png")
+
+func _ready():
+	Input.set_custom_mouse_cursor(arrow)
+	
+	start_button.button_down.connect(on_start_pressed)
+	settings_button.button_down.connect(on_settings_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(start_level)
+	
+
+func on_settings_pressed() -> void:
+	pass
+	
+func on_exit_pressed() -> void:
+	get_tree().quit()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
