60b587ca56f5d8161e15a366e57c962c817fdaf0
Alexeev Eduard <47676479+MR-d3R@users.noreply.github.com>
2024-03-27 19:12:52 +0300

    changed contols to WASD

---
+++ scenes/hotkey_rebind_button.gd
@@ -0,0 +1,15 @@
+class_name HotkeyRebindButton
+extends Control
+
+@onready var label = $HBoxContainer/Label as Label
+@onready var button = $HBoxContainer/Button as Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
