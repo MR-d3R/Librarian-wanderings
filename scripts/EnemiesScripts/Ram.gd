3fe9b972fe7f7e91d6d7dd2b77d54756d745769d
Alexeev Eduard <47676479+MR-d3R@users.noreply.github.com>
2024-04-04 22:12:27 +0300

    Added layers, hitboxes and hurtboxes

--- scripts/EnemiesScripts/Ram.gd
+++ scripts/EnemiesScripts/Ram.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
-
 @export var SPEED = -300.0
 #@export var JUMP_VELOCITY = -400.0
 var facin_right = false
 var player
 var direction
+var max_hp = 100
+var hp = max_hp
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var collision =  $Hitbox/CollisionShape2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -21,12 +22,13 @@ func _physics_process(delta):
 	
 func get_direction():
 	player = get_parent().get_node("Player")
-	if player.get_node("CollisionShape2D").global_position.x < $CollisionShape2D.global_position.x-22:
+	if player.get_node("CollisionShape2D").global_position.x < collision.global_position.x-22:
 		direction = 1
-	elif player.get_node("CollisionShape2D").global_position.x > $CollisionShape2D.global_position.x-22:
+	elif player.get_node("CollisionShape2D").global_position.x > collision.global_position.x-22:
 		direction = -1
 	else:
 		direction = 0
+
 func flip():
 	facin_right = !facin_right
 	scale.x = abs(scale.x) * -1
@@ -34,3 +36,9 @@ func flip():
 		SPEED = abs(SPEED)
 	else:
 		SPEED = abs(SPEED) * -1
+
+
+func _on_hitbox_area_entered(hitbox):
+	var base_damage = hitbox.damage 
+	self.hp -= base_damage
+	print(hitbox.get_parent().name + "'s hitbox touched " + name + "'s hurtbox and dealt " + str(base_damage))
