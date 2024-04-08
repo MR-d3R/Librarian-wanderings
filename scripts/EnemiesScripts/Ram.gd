39e3675724681e68cdb35bfe1070b6d59d258960
KaverinTihon <tihon27062004@yandex.ru>
2024-04-08 14:21:37 +0500

    ITS FINALY FLIPIN
    he does the flip OOOOOOOOOOOOOOOOOOOO

--- scripts/EnemiesScripts/Ram.gd
+++ scripts/EnemiesScripts/Ram.gd
@@ -1,8 +1,7 @@
 extends CharacterBody2D
 
-@export var SPEED = -300.0
+@export var SPEED = 100.0
 #@export var JUMP_VELOCITY = -400.0
-var facin_right = false
 var player
 var direction
 var max_hp = 100
@@ -17,27 +16,29 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	get_direction()
-	velocity.x = SPEED * direction
+	if direction != 0:
+		velocity.x = SPEED * direction
+		if direction == -1:
+			$Sprite2D.flip_h = false
+		else:
+			$Sprite2D.flip_h = true
+	else:
+		attack()
 	move_and_slide()
 	
 func get_direction():
 	player = get_parent().get_node("Player")
-	if player.get_node("CollisionShape2D").global_position.x < collision.global_position.x-22:
-		direction = 1
-	elif player.get_node("CollisionShape2D").global_position.x > collision.global_position.x-22:
+	var player_pos = player.get_node("CollisionShape2D").global_position.x
+	var enemy_pos = collision.global_position.x
+	
+	if player_pos + 100 < enemy_pos:
 		direction = -1
+	elif player_pos - 100 > enemy_pos:
+		direction = 1
 	else:
 		direction = 0
-
-func flip():
-	facin_right = !facin_right
-	scale.x = abs(scale.x) * -1
-	if facin_right:
-		SPEED = abs(SPEED)
-	else:
-		SPEED = abs(SPEED) * -1
-
-
+func attack():
+	pass
 func _on_hitbox_area_entered(hitbox):
 	var base_damage = hitbox.damage 
 	self.hp -= base_damage
