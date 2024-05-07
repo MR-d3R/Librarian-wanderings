a12ace9e278f61553bda6dcf37a3301e6f0239d7
Alexeev Eduard <47676479+MR-d3R@users.noreply.github.com>
2024-05-07 04:39:09 +0300

    projectile destroys on when enters ram

--- scripts/EnemiesScripts/Ram.gd
+++ scripts/EnemiesScripts/Ram.gd
@@ -9,17 +9,19 @@ var direction = 1
 var max_hp = 100
 var hp = max_hp
 var facin_right = true
+var knockback_strength = 64
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var collision =  $Hitbox/CollisionShape2D
+@onready var attack_timer = $AttackTimer
 
 func _physics_process(delta):
 	get_direction()
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
-		velocity.x = SPEED * direction
+		velocity.x = SPEED * direction	
 	flip()
 	move_and_slide()
 	
@@ -52,6 +54,19 @@ func attack():
 	
 func _on_hitbox_area_entered(hitbox):
 	receive_damage(hitbox)
+	var hit_direction = global_position.direction_to(collision)
+	velocity -= hit_direction * knockback_strength
 	
 	if hitbox.is_in_group("Projectile"):
 		hitbox.destroy()
+
+
+func _on_hurt_box_area_entered(area):
+	receive_damage(area)
+	player = get_parent().get_node("Player")
+	var player_to_bullet = player.global_position.direction_to(area.global_position)
+	var knockback = player_to_bullet * knockback_strength
+	velocity -= knockback
+	
+	if area.is_in_group("Projectile"):
+		area.destroy()
