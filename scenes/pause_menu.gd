52c7e4642c193ba567736ec9d0558a74234cc70f
Ayaz <zan.rt2016@gmail.com>
2024-05-09 11:56:23 +0300

    added pause_menu

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -15,7 +15,10 @@ func _on_game_toggle_is_paused(game_paused: bool):
 	else:
 		hide()
 
+func _on_resume_button_pressed():
+	game_manager.game_paused = false
 
 
-#func _on_exit_button_clicked():
-	#get_tree().change_scene("res://scenes/main_menu.tscn")
+func _on_exit_to_menu_button_pressed():
+	game_manager.game_paused = false
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
